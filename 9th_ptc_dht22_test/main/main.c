89a32079974d891130191b00471ed31d730f6a7d
schronding <schronding@gmail.com>
2026-04-22 14:05:07 -0600

    Added UART serial port to 9th practice

--- 9th_ptc_dht22_test/main/main.c
+++ 9th_ptc_dht22_test/main/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_log.h"
 #include "dht.h" 
+
 #define SENSOR_TYPE DHT_TYPE_AM2301 
 #define SENSOR_GPIO 32              
 #define LED_GPIO 2
 #define BASE_STACK_SIZE 1024        
+
+#define UART_PORT_NUM      UART_NUM_1
+#define UART_BAUD_RATE     115200
+#define UART_TXD           1
+#define UART_RXD           3
+#define UART_RTS           UART_PIN_NO_CHANGE
+#define UART_CTS           UART_PIN_NO_CHANGE
+#define BUF_SIZE           1024
 
 static const char *TAG = "DHT_XX_TEST";
 
@@ -15,6 +26,7 @@ void dht_test_task(void *pvParameters)
 {
     (void)pvParameters;
     float temperature, humidity;
+    char uart_msg[64];
 
     while (1)
     {
@@ -22,12 +34,13 @@ void dht_test_task(void *pvParameters)
 
         if (res == ESP_OK)
         {
-            ESP_LOGI(TAG, "Humidity: %.1f%% | Temperature: %.1f C", humidity, temperature);
+            ESP_LOGI(TAG, "%.1f,%.1f", temperature, humidity);
+            int len = sprintf(uart_msg, "%.1f,%.1f\r\n", temperature, humidity);
+            uart_write_bytes(UART_PORT_NUM, uart_msg, len);
         }
         else
         {
             ESP_LOGE(TAG, "Sensor read error (Code: %d)", res);
-            printf("SERIAL -> Sensor read error (Code: %d)\n", res);
         }
 
         vTaskDelay(pdMS_TO_TICKS(2000));
@@ -49,6 +62,24 @@ void led_signal_task(void *pvParameters)
 
 void app_main(void)
 {
+    uart_config_t uart_config = {
+        .baud_rate = UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    int intr_alloc_flags = 0;
+
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_PORT_NUM, UART_TXD, UART_RXD, UART_RTS, UART_CTS));
+
     gpio_reset_pin(LED_GPIO);
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 
